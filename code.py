746d9fafe1b313e0d7abd0427caa51a53ada5019
mixamart <mixamart98@gmail.com>
2022-11-12 17:54:02 +0200

    Added print board

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
 import os
 
-
-
 ### this is a method to clear the diplay of our console
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+def clear_disp():
+    os.system('clear' if os.name=='nt' else 'clear')
 
-# now, to clear the screen
-cls()
+### Build our display
+def display_board(board):
+    clear_disp()
+    print(board[7] + "|" + board[8] + "|" + board[9])
+    print('-----')
+    print(board[4] + "|" + board[5] + "|" + board[6])
+    print('-----')
+    print(board[1] + "|" + board[2] + "|" + board[3])
+
+demo_board = ['#','X','O','X','O','X','O','X','O','X']
+display_board(demo_board)
+
+
+
