6907ba66d655e7cb9f194d5d2a9e877382ec0ef4
mixamart <mixamart98@gmail.com>
2022-11-12 18:11:36 +0200

    Added player input function

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import os
 
-### this is a method to clear the diplay of our console
+### Clear the Console-Display function
 def clear_disp():
     os.system('clear' if os.name=='nt' else 'clear')
 
@@ -16,5 +16,21 @@ def display_board(board):
 demo_board = ['#','X','O','X','O','X','O','X','O','X']
 display_board(demo_board)
 
+### Get Player Input
+def player_input():
+    marker = ''
+    
+    while marker != "X" and marker != "O":
+        marker = input ('Player 1 please choose X or O as your move: ').upper()
+        # .upper in order not to have an extra check for small x or o
+
+        if marker == "X":
+            return ('X','O')
+        elif marker == "O":
+            return ('O','X')
+        else:
+            print(f"{marker} is not a valid option. Please type X or O")
+player1_marker, player2_marker = player_input()
+print(f"The player 1 has marker {player1_marker}, The player 2 has marker {player2_marker}")
 
 
