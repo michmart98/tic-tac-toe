10d14fe370f5f3fe8c2d4ea6fb384ef8b1701897
mixamart <mixamart98@gmail.com>
2022-11-12 17:35:44 +0200

    an addition for feature use, build a mechanism to clear the display

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
+import os
+
 print("this is just a test")
+
+### this is a method to clear the diplay of our console
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+# now, to clear the screen
+cls()
